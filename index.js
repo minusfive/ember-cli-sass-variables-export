28560e668ded5d3527c19c4a4e214738c7904fad
minusfive <mail@minusfive.com>
2018-06-05 14:15:52 -0400

    Simplifed sassOptions config, cleaned-up exportSass function

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
 'use strict';
 const exportSass = require('./lib/exportSass');
-const findHost = require('./lib/findHost');
+const path = require('path');
 
 module.exports = {
   name: 'ember-export-sass-variables',
 
-  included(parent) {
-    this._super.included.apply(this, arguments);
-    let host = findHost(parent);
-
-    if (!host) {
-      host = parent;
+  config(env, baseConfig) {
+    return {
+      sassOptions: {
+        functions: exportSass(path.join(__dirname, 'addon', 'utils'))
+      }
     }
-
-    // https://github.com/aexmachina/ember-cli-sass/issues/171
-    // You get this deprecation warning, but we shouldn't see this in an addon
-    if (!host.options.sassOptions) {
-      host.options.sassOptions = {};
-    }
-
-    if (!host.options.sassOptions.functions) {
-      host.options.sassOptions.functions = {};
-    }
-
-    Object.assign(host.options.sassOptions.functions, exportSass(`${__dirname}/addon/utils`));
-    return parent;
-  },
+  }
 };
