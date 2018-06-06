fe69b1618f2dce6eef28e7b5473cb4723473d3e3
minusfive <mail@minusfive.com>
2018-06-06 17:22:57 -0400

    Attempt to get Travis build to pass

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   name: 'ember-cli-sass-variables-export',
 
-  config() {
+  config(/* env, baseConfig */) {
     return {
       sassOptions: {
         functions: exportSass(path.join(__dirname, 'addon', 'utils'))
