3773085bdd4f1b279faa2c021134d83ef2a526ee
minusfive <mail@minusfive.com>
2018-06-06 15:13:51 -0400

    Reset eslint env on exportSass, remove unused params from config hook on index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   name: 'ember-cli-sass-variables-export',
 
-  config(env, baseConfig) {
+  config() {
     return {
       sassOptions: {
         functions: exportSass(path.join(__dirname, 'addon', 'utils'))
