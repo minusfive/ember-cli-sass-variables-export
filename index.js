003f5ab597e46fc5a6a1fd070d2b401ad9ee034b
minusfive <mail@minusfive.com>
2018-09-04 23:20:47 -0400

    Switch sassOptions to included hook while warning is removed from ember-cli-sass

--- index.js
+++ index.js
@@ -5,11 +5,31 @@ const path = require('path');
 module.exports = {
   name: 'ember-cli-sass-variables-export',
 
-  config(/* env, baseConfig */) {
-    return {
-      sassOptions: {
-        functions: exportSass(path.join(__dirname, 'addon', 'utils'))
-      }
-    };
+  included(/* app */) {
+    this._super.included.apply(this, arguments);
+    let app;
+
+    // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
+    // use that.
+    if (typeof this._findHost === 'function') {
+      app = this._findHost();
+    } else {
+      // Otherwise, we'll use this implementation borrowed from the _findHost()
+      // method in ember-cli.
+      let current = this;
+      do {
+        app = current.app || app;
+      } while (current.parent && current.parent.parent && (current = current.parent));
+    }
+
+    if (!app.options.sassOptions) {
+      app.options.sassOptions = {};
+    }
+
+    if (!app.options.sassOptions.functions) {
+      app.options.sassOptions.functions = {};
+    }
+
+    Object.assign(app.options.sassOptions.functions, exportSass(path.join(__dirname, 'addon', 'utils')));
   }
 };
